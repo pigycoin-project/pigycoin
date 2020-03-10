10b38e8cdbed4f322da140a5e8a9dc30fd193c62
Pigycoin Core Developer <61416421+pigycoin-project@users.noreply.github.com>
2020-03-10 07:44:19 -0800

    Add files via upload

---
+++ src/script/ismine.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2020 Wayan Saka
+// Copyright (c) 2020 The Pigycoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#ifndef PIGYCOIN_SCRIPT_ISMINE_H
+#define PIGYCOIN_SCRIPT_ISMINE_H
+
+#include <script/standard.h>
+
+#include <stdint.h>
+
+class CKeyStore;
+class CScript;
+
+/** IsMine() return codes */
+enum isminetype
+{
+    ISMINE_NO = 0,
+    ISMINE_WATCH_ONLY = 1,
+    ISMINE_SPENDABLE = 2,
+    ISMINE_ALL = ISMINE_WATCH_ONLY | ISMINE_SPENDABLE
+};
+/** used for bitflags of isminetype */
+typedef uint8_t isminefilter;
+
+isminetype IsMine(const CKeyStore& keystore, const CScript& scriptPubKey);
+isminetype IsMine(const CKeyStore& keystore, const CTxDestination& dest);
+
+#endif // PIGYCOIN_SCRIPT_ISMINE_H
