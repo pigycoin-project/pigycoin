fb07416406f56f144d7918ea7410fc495440b09f
Pigycoin Core Developer <61416421+pigycoin-project@users.noreply.github.com>
2020-03-10 08:02:38 -0800

    Add files via upload

---
+++ src/qt/test/addressbooktests.h
@@ -0,0 +1,15 @@
+#ifndef PIGYCOIN_QT_TEST_ADDRESSBOOKTESTS_H
+#define PIGYCOIN_QT_TEST_ADDRESSBOOKTESTS_H
+
+#include <QObject>
+#include <QTest>
+
+class AddressBookTests : public QObject
+{
+    Q_OBJECT
+
+private Q_SLOTS:
+    void addressBookTests();
+};
+
+#endif // PIGYCOIN_QT_TEST_ADDRESSBOOKTESTS_H
---
+++ src/qt/test/util.h
@@ -0,0 +1,12 @@
+#ifndef PIGYCOIN_QT_TEST_UTIL_H
+#define PIGYCOIN_QT_TEST_UTIL_H
+
+/**
+ * Press "Ok" button in message box dialog.
+ *
+ * @param text - Optionally store dialog text.
+ * @param msec - Number of miliseconds to pause before triggering the callback.
+ */
+void ConfirmMessage(QString* text = nullptr, int msec = 0);
+
+#endif // PIGYCOIN_QT_TEST_UTIL_H
